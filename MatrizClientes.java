4100f6b854e45f895b81ee37660830f79263abfb
Matheus Bittencourt <matheuspb2005@hotmail.com>
2015-11-09 23:17:15 -0200

    criado MatrizClientes

--- MatrizClientes.java
+++ MatrizClientes.java
@@ -1,4 +1,18 @@
-
 public class MatrizClientes {
+	
+	Cliente[][] matriz;
 
+	public MatrizClientes(int linhas, int colunas) {
+		super();
+		matriz = new Cliente[linhas][colunas];
+	}
+	
+	public void colocaCliente(Cliente cliente, int linha, int coluna) {
+		matriz[linha][coluna] = cliente;
+	}
+	
+	public Cliente pegaCliente(int linha, int coluna) {
+		return matriz[linha][coluna];
+	}
+	
 }
